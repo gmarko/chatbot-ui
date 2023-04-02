d9afdf4c17e5db8cb585664c435122002d691d33
jponline77 <jponline77@hotmail.com>
2023-04-02 17:56:31 +0000

    Changed to Netint title

--- pages/index.tsx
+++ pages/index.tsx
@@ -626,8 +626,8 @@ const Home: React.FC<HomeProps> = ({
   return (
     <>
       <Head>
-        <title>Chatbot UI</title>
-        <meta name="description" content="ChatGPT but better." />
+        <title>Netint GPT</title>
+        <meta name="description" content="Netint GPT, changing the world today." />
         <meta
           name="viewport"
           content="height=device-height ,width=device-width, initial-scale=1, user-scalable=no"
